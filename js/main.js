02e5eab9830bbb3066b8655d89e82d0c4b6a205c
coachjanus <coach.qalight@gmail.com>
2025-06-16 21:08:54 +0300

    added store

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 
-
-
-// document.addEventListener("DOMContentLoaded", messageShow)
-
-
-
-
 const appNav = document.querySelector('.app-nav');
 const appNavHide = document.querySelector('.app-nav--hide');
 const appNavShow = document.querySelector('.app-nav--show');
@@ -15,7 +8,32 @@ const hamburger = document.getElementById('hamburger');
 
 const productContainer = document.querySelector('.product-container');
 
-const addToCartButtons = productContainer.querySelectorAll('.add-to-cart');
+
+
+class Store {
+
+
+    static get (key) {
+        let value = localStorage.getItem(key);
+        return value === null ? null : JSON.parse(value);
+    }
+
+    static set (key, value) {
+        return localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    static init(key) {
+        if (!Store.isset(key)) {
+            Store.set(key, []);
+        }
+        return Store.get(key);
+    }
+
+    static isset(key) {
+        return Store.get(key) !== null;
+    }
+}
+
 
 const hideNav = () => {
     appNav.classList.add('app-nav__hide');
@@ -38,37 +56,74 @@ function initNav() {
     hamburger.addEventListener('touchted', showNav);
 }
 
+const productItemTemplate = (product) => `
+<div class="card card-badge badge-${product.badge}" data-id="${product.id}">
+    <div class="card-figure">
+        <div class="icons">
+            <a href="#!" class="fas fa-shopping-cart add-to-cart"></a>
+            <a href="#" class="fas fa-heart"></a>
+            <a href="#" class="fas fa-eye"></a>
+        </div>
+        <figure class="image">
+            <img src="${product.image}" alt="${product.name}">
+        </figure>
+    </div>
+    <div class="card-body" data-title="Black Cat">
+        <div class="card-title">${product.name}</div>
+        <span class="card-price">${product.price}</span>
+    </div>
+</div>`;
+
+
+const populateProductList = () => {
+    let content = "";
+    products.forEach(item => content += productItemTemplate(item));
+    return content;
+}
 
 function main() {
     initNav();
 
-    // console.dir(addToCartButtons);
+    productContainer.innerHTML = populateProductList();
 
-    // for (let i=0; i<addToCartButtons.length; i++) {
-    //     console.log(addToCartButtons[i]);
-    // }
+    const addToCartButtons = productContainer.querySelectorAll('.add-to-cart');
+
+    // let shoppingCart = [];
+    let shoppingCart = Store.init('basket');
+
+
+    function saveCart() {
+        Store.set('basket', shoppingCart);
+    }
+
 
     addToCartButtons.forEach(item => {
         item.addEventListener('click', (event) => {
-            // console.log(item);
-
-            // console.log(event.target);
+            
             let product = event.target.closest('.card');
-            // console.log(product);
-            console.log(product.children[1].children[1].innerHTML);
-            console.log(product.children[1].children[1].innerText);
-            console.log(product.children[1].children[0]);
-            console.log(product.children[1].dataset.title);
+            // console.log(product.dataset.id);
+            let id = product.dataset.id;
+            // console.log(products[id]);
+            let item = {
+                id: id,
+                amount: 1
+            };
+            shoppingCart.push(item);
+
+            saveCart();
+            
         });
     });
+
+    // console.log(shoppingCart);
 
 }
 
 (
     () => {
-        if(document.readyState === "loading") {
+        if (document.readyState === "loading") {
             document.addEventListener('DOMContentLoaded', main);
-        }else{
+        } else {
             main();
         }
     }
